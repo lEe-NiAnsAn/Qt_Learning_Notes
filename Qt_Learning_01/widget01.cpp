cb51cbe9c670c67f1361bd9803d80bf45400a222
lEe_NiAnsAn <lEe-NiAnsAn@github.com>
2024-09-23 09:42:21 +0800

    简单来设置第一个小按钮

--- Qt_Learning_01/widget01.cpp
+++ Qt_Learning_01/widget01.cpp
@@ -1,7 +1,14 @@
 #include "widget01.h"
+#include <QPushButton>
 
 Widget01::Widget01(QWidget* parent)
     : QWidget(parent)   //初始化参数列表
-{}
+{
+    //Qt中功能通常在窗口类的构造函数中实现
+    QPushButton* btn = new QPushButton; //需包含对应头文件
+    //btn->show();    //以顶层方式显示(单独窗口)
+    btn->setParent(this);   //设置父窗口，参数：指定父窗口的指针
+
+}
 
 Widget01::~Widget01() {}
